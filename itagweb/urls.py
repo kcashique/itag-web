8114ab1569f6637fb82319abcf2ea40d4602e10c
kcashique <ashiquekc97@gmail.com>
2024-02-26 16:54:29 +0530

    views and urls configuratuions done half

--- itagweb/urls.py
+++ itagweb/urls.py
@@ -1,6 +1,19 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
 
-urlpatterns = [
+
+
+urlpatterns = (
+    [
     path("admin/", admin.site.urls),
-]
+    path("", include("web.urls", namespace="web")),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
+
+admin.site.site_header = "Itag Administration"
+admin.site.site_title = "Itag Admin Portal"
+admin.site.index_title = "Welcome to Itag Admin Portal"
--- web/views.py
+++ web/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    template_name = "index-7.html"
+    # context ={
+    #     "is_index" : True,
+    # }
+    return render(request, template_name)
